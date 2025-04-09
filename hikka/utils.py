408cfd7785f1e06ccbef76bfc8a81968040da2d9
xdesai <samirgamzaev7@gmail.com>
2025-04-09 21:58:45 +0200

    fix: build in info

--- hikka/utils.py
+++ hikka/utils.py
@@ -1298,7 +1298,7 @@ def get_commit_url() -> str:
     """
     try:
         hash_ = get_git_hash()
-        return f'<a href="https://github.com/coddrago/Heroku/commit/{hash_}">#{hash_[:7]}</a>'
+        return f'<a href="https://github.com/Crayz310/Heroku-Legacy/commit/{hash_}">#{hash_[:7]}</a>'
     except Exception:
         return "Unknown"
 
