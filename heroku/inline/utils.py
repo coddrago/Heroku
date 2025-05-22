09f1d40c33418818b851d75073ef470e8ca11255
ZetGo | Aleksej K. <73279716+ZetGoHack@users.noreply.github.com>
2025-05-22 23:07:11 +0300

    markup generator(pydantic 🤘)

--- heroku/inline/utils.py
+++ heroku/inline/utils.py
@@ -60,7 +60,7 @@ class Utils(InlineUnit):
         if isinstance(markup_obj, InlineKeyboardMarkup):
             return markup_obj
 
-        markup = InlineKeyboardMarkup()
+        markup = InlineKeyboardMarkup(inline_keyboard=[])
 
         map_ = (
             self._units[markup_obj]["buttons"]
