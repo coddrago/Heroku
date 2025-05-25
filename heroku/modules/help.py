5d88b718e3214639f372960ee8c2f22dc2d8cdb5
ZetGo | Aleksej K. <73279716+ZetGoHack@users.noreply.github.com>
2025-05-25 12:09:17 +0300

    [test] Is it really that hard?

--- heroku/modules/help.py
+++ heroku/modules/help.py
@@ -333,7 +333,7 @@ class Help(loader.Module):
 
         await utils.answer(
             message,
-            (self.config["desc_icon"] + " {}\n <blockquote>{}</blockquote><blockquote>{}</blockquote>").format(
+            (self.config["desc_icon"] + " {}\n <blockquote expandable>{}</blockquote expandable><blockquote>{}</blockquote>").format(
                 reply,
                 "".join(core_ + plain_ + (no_commands_ if force else [])),
                 (
