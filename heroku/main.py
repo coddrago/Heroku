cac1374e082e53e3926d35a811bb80d5e1c08a68
Codrago | Tosha C. <155328415+coddrago@users.noreply.github.com>
2026-01-23 06:40:08 +0000

    not update pls

--- heroku/main.py
+++ heroku/main.py
@@ -977,7 +977,7 @@ class Heroku:
                         logging.error(f"Exception on loading allowed beta testers ids: {response.status}")
                         return []
 
-            await asyncio.gather(*[version.check_branch((await client.get_me()).id, allowed_ids) for client in self.clients])
+            await asyncio.gather(*[version.check_branch((await client.get_me()).id, allowed_ids) for client in self.clients], self)
 
             while await self.amain(first, client):
                 first = False
