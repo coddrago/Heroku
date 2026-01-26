3a62472d2c0f2c0625cae971f046fe3b4500096c
Codrago | Tosha C. <155328415+coddrago@users.noreply.github.com>
2026-01-26 15:26:38 +0700

    Update dispatcher.py

--- heroku/dispatcher.py
+++ heroku/dispatcher.py
@@ -352,9 +352,9 @@ class CommandDispatcher:
 
 
         command = message.message[len(prefix):].strip().split(maxsplit=1)[0]
-        logger.info(f"Received command: {command}")
+        #logger.info(f"Received command: {command}")
         tag = command.split("@", maxsplit=1)
-        logger.info(f"Command tag: {tag}")
+        #logger.info(f"Command tag: {tag}")
 
         if len(tag) == 2:
             if tag[1] == "me":
