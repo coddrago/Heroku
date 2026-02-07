8fcdce511da57f79d0aa0a72cb08f3ead962bd81
Midga3 <mikhaylodm@yandex.ru>
2026-02-07 10:45:18 +0000

    test 6

--- heroku/modules/presets.py
+++ heroku/modules/presets.py
@@ -335,6 +335,7 @@ class Presets(loader.Module):
             logger.exception("Invalid preset format")
             return
         await message.delete()
-        call = await self.inline.form(message=message, text=utils.ascii_face())
-        self._install(call=call, preset=data["name"], modules=data["links"], origin=False)
-        
+        call = await self.inline.form(message=message, text=self.lookup("api_protection").strings['u_sure'], reply_markup=[
+            {"text": self.strings("install"), "callback": self._install, "args": (call, data["name"], data["links"], False)},
+            {"text": self.lookup("settings").strings["cancel"], "callback": lambda call: call.delete()},
+        ])
