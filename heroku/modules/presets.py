f984d64feac6406f404ee5e467b43588b75a0f31
Midga3 <mikhaylodm@yandex.ru>
2026-02-07 10:49:11 +0000

    test 8

--- heroku/modules/presets.py
+++ heroku/modules/presets.py
@@ -336,6 +336,6 @@ class Presets(loader.Module):
             return
         await message.delete()
         await self.inline.form(message=message, text=self.lookup("api_protection").strings['u_sure'], reply_markup=[
-            {"text": self.strings("install"), "callback": self._install, "args": (data["name", data["links"], False)},
+            {"text": self.strings("install"), "callback": self._install, "args": (data["name"], data["links"], False)},
             {"text": self.lookup("settings").strings["cancel"], "callback": lambda call: call.delete()},
         ])
