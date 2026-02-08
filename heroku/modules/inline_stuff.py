9b7dce5ae8af3a2b2859b7d6165c6d98467f89d9
Midga3 <mikhaylodm@yandex.ru>
2026-02-08 09:51:52 +0000

    add premium emoji in start message on bot

--- heroku/modules/inline_stuff.py
+++ heroku/modules/inline_stuff.py
@@ -116,7 +116,7 @@ class InlineStuff(loader.Module):
             case "/start":
                 await message.answer_photo(
                     "https://raw.githubusercontent.com/coddrago/assets/refs/heads/main/heroku/start_cmd.png",
-                    caption=self.strings("this_is_heroku"),
+                    caption=self.strings("this_is_heroku").format("<tg-emoji emoji-id=5463379725441341739>🪐</tg-emoji>" if self._client.heroku_me.premium else "🪐", utils.get_platform_emoji() if self._client else "Heroku"),
                 )
             case "/profile":
                 if message.from_user.id != self.client.tg_id:
