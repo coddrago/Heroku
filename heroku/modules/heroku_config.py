becfb9fbeda2b809e72499af2fb191e280514627
Midga3 <mikhaylodm@yandex.ru>
2026-02-09 18:44:02 +0000

    fix cfg

--- heroku/modules/heroku_config.py
+++ heroku/modules/heroku_config.py
@@ -928,7 +928,7 @@ class HerokuConfigMod(loader.Module):
         try:
             from . import presets as _presets_mod
 
-            preset_folders = getattr(_presets_mod, "FOLDERS", {}) or {}
+            preset_folders = self.db.get(_presets_mod.__name__, "folders")
         except Exception:
             preset_folders = {}
 
