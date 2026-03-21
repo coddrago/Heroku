10eb5b9ba8c2a24783c5db6c01282c77da77da3e
Midga3 <mikhaylodm@yandex.ru>
2026-03-21 18:30:31 +0300

    revert lol

--- heroku/modules/heroku_info.py
+++ heroku/modules/heroku_info.py
@@ -50,7 +50,7 @@ class HerokuInfoMod(loader.Module):
                     "<blockquote expandable>"
                     + self.strings("_cfg_cst_msg")
                     + "\n"
-                    + self.strings("_cfg_cst_ph").format("\n"+utils.config_placeholders()) if utils.config_placeholders() else ""
+                    + self.strings("_cfg_cst_ph").format(utils.config_placeholders()) if utils.config_placeholders() else ""
                     + "</blockquote>"
                 ),
             ),
