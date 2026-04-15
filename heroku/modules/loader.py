1096eed8ff845977b08283327433e2c32d235440
coddrago <drakongg1337@gmail.com>
2026-04-15 23:40:24 +0700

    /

--- heroku/modules/loader.py
+++ heroku/modules/loader.py
@@ -1412,7 +1412,7 @@ class LoaderMod(loader.Module):
 
         for file in os.scandir(loader.LOADED_MODULES_DIR):
             try:
-                shutil.rmtree(file.path)
+                os.remove(file.path)
             except Exception:
                 logger.debug("Failed to remove %s", file.path, exc_info=True)
 
