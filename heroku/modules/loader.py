76cf4772a38ff99fbdc76ad0bfb9bd2c9a97c798
FireJester <Van.anime.maa@icloud.com>
2026-04-15 17:33:41 +0000

    fix getting args

--- heroku/modules/loader.py
+++ heroku/modules/loader.py
@@ -205,7 +205,7 @@ class LoaderMod(loader.Module):
 
         if args := utils.get_args(message):
             match args:
-                case ["-all"]:
+                case [single] if single.lstrip("-").lower() == "all":
                     repos = [self.config["MODULES_REPO"]] + self.config[
                         "ADDITIONAL_REPOS"
                     ]
