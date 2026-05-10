cb07fe3cb76294c536a15db265990678301441b6
ZetGo | Aleksej K. <ZetGoHack@users.noreply.github.com>
2026-05-10 16:13:23 +0300

    improve unknown string handling
    (cherry picked from commit 67cb66e528e0bf3951f020a9351e0e394bc8660e)

--- heroku/translations.py
+++ heroku/translations.py
@@ -241,7 +241,7 @@ class Strings:
                 if self._translator is not None
                 else self._base_strings.get(key)
             )
-            or self._base_strings.get(key, "Unknown strings")
+            or self._base_strings.get(key, f"Unknown strings: {key}")
         )
 
     def __call__(
